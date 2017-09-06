b70637b4352b2465c577351030add3fa6bf29303
BigMarbz <matthewwang2020@u.northwestern.edu>
2017-09-06 17:06:14 -0400

    Capacitance Logic

--- Startup/Capacitancce/Capacitancce.ino
+++ Startup/Capacitancce/Capacitancce.ino
@@ -1,16 +1,18 @@
+#include <CapacitiveSensor.h>
+
 void setup() {
   CapacitiveSensor cap12 = CapacitiveSensor(1,2);
-  CapacitiveSensor cap34 = CapacitiveSensor(3,4);
+  CapacitiveSensor cap34 = CapacitiveSensor(1,3);
   unsigned long cs1=0;
   unsigned long cs2=0;
   int channel1 = 0;
   int channel2 = 0;
   int countc1 = 0;
   int countc2 = 0;
+  Serial.begin(9600);
 }
 
 void loop() {
- Serial.begin(9600);
  checkc1();
 }
 
@@ -52,38 +54,47 @@ void sendsignal1(){
   switch(channel1){
   case 1:
   // Sends signal of 1 to the TV
+  sendsignal2();
   break;
 
   case 2:
   // Sends signal of 2 to the TV
+  sendsignal2();
   break;
 
   case 3:
   // Sends signal of 3 to the TV
+  sendsignal2();
   break;
 
   case 4:
   // Sends signal of 4 to the TV
+  sendsignal2();
   break;
 
   case 5:
   // Sends signal of 5 to the TV
+  sendsignal2();
   break;
 
   case 6:
   // Sends signal of 6 to the TV
+  sendsignal2();
   break;
 
  case 7:
   // Sends signal of 7 to the TV
+  sendsignal2();
   break;
 
  case 8:
   // Sends signal of 8 to the TV
+  sendsignal2();
   break;
 
  case 9:
   // Sends signal of 9 to the TV
+  sendsignal2();
   break;
   
   }
@@ -93,6 +104,7 @@ void sendsignal2(){
     switch(channel2){
   case 1:
   // Sends signal of 1 to the TV
+  resetvalues();
   break;
 
   case 2:
@@ -127,6 +139,18 @@ void sendsignal2(){
   // Sends signal of 9 to the TV
   break;
   
-  
+}
+}
+
+void resetvalues(){
+  cs1=0;
+  cs2=0;
+  channel1 = 0;
+  channel2 = 0;
+  countc1 = 0;
+  countc2 = 0;
+  delay(1000);
+  loop();
+}
 }
 
